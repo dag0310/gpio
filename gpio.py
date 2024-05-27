4b49ed253d32d67406052a7dcbd87f76bb62d605
Daniel Geymayer <dag0310@gmail.com>
2024-05-27 16:11:47 +0200

    Catch exceptions if network is unreachable for example to keep the script running

--- gpio.py
+++ gpio.py
@@ -16,15 +16,23 @@ API_URL = 'http://retropie:3000/udp?command='
 try:
     print("Receiving button state ...\n")
     while True:
-        if GPIO.input(GPIO_BUTTON_UP) == GPIO.LOW:
-            requests.post(f"{API_URL}shutters_up")
-            print("Button UP pressed.")
-            time.sleep(1)
-        if GPIO.input(GPIO_BUTTON_DOWN) == GPIO.LOW:
-            requests.post(f"{API_URL}shutters_down")
-            print("Button DOWN pressed.")
+        try:
+            if GPIO.input(GPIO_BUTTON_UP) == GPIO.LOW:
+                requests.post(f"{API_URL}shutters_up")
+                print("Button UP pressed.")
+                time.sleep(1)
+            if GPIO.input(GPIO_BUTTON_DOWN) == GPIO.LOW:
+                requests.post(f"{API_URL}shutters_down")
+                print("Button DOWN pressed.")
+                time.sleep(1)
+        except Exception as e:
+            print(f"An error occurred: {e}")
             time.sleep(1)
         time.sleep(0.1)
 except KeyboardInterrupt:
     print("\Script stopped by user.")
+except Exception as e:
+    print(f"An error occurred: {e}")
+finally:
     GPIO.cleanup()
+
